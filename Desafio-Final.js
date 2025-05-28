2477c6ccfe93bd9e3bd98b69afd0387300e6de37
Nicolly Roberta Morethes <nrmorethes@gmail.com>
2025-05-28 11:35:21 -0300

    Add files via upload

---
+++ Desafio-Final.js
@@ -0,0 +1,27 @@
+class UserHero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.ataque = {
+            mago: "magia",
+            guerreiro: "espada",
+            monge: "artes marciais",
+            ninja: "shuriken"
+        };
+    }
+    
+    escreva(){
+        let ataque = "ataque desconhecido";
+        for (let tipo in this.ataque){
+            if (tipo === this.tipo){
+                ataque = this.ataque[tipo];
+                break;
+                }
+            }
+        console.log(`${this.nome} é ${this.tipo} e atacou usando ${ataque}`);
+            }
+
+ }
+    let usuario = new UserHero("Nicolly", 27, "mago");
+usuario.escreva(); 
